051e119f015e00c0ae490c566fce16fef9b26dd2
kturchik <kturchik@sbcglobal.net>
2018-02-03 17:23:51 -0800

    Added Photon, basic server connectivity

---
+++ Assets/Scripts/PhotonNetworkManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhotonNetworkManager : Photon.MonoBehaviour
+{
+    [SerializeField]
+    private Text connectText;
+
+	// Use this for initialization
+	void Start () {
+        PhotonNetwork.ConnectUsingSettings("0.1"); // Manages version, only players with same game version can connect
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // NOTE: FOR TESTING ONLY
+        connectText.text = PhotonNetwork.connectionStateDetailed.ToString();
+	}
+}
