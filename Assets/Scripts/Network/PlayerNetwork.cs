242672a91ab55b9d3dc673b57203811f525d6e6b
kturchik <kturchik@sbcglobal.net>
2018-02-03 19:55:39 -0800

    Working multiplayer network

---
+++ Assets/Scripts/Network/PlayerNetwork.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerNetwork : MonoBehaviour
+{
+    [SerializeField] private GameObject playerCamera;
+    [SerializeField] private MonoBehaviour[] playerControlScripts;
+
+    private PhotonView photonView;
+
+
+	// Use this for initialization
+	private void Start ()
+    {
+        photonView = GetComponent<PhotonView>();
+
+        InitializePlayer();
+	}
+
+    private void InitializePlayer()
+    {
+        if (photonView.isMine)
+        {
+            // Do stuff
+        }
+        // Handle functionality for non local character
+        else
+        {
+            // Disable it's camera
+            playerCamera.SetActive(false);
+
+            // Disable it's control scripts
+            foreach(MonoBehaviour m in playerControlScripts)
+            {
+                m.enabled = false;
+            }
+        }
+    }
+}
